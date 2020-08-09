ebfdb793674cda4145766f297ebd8b4d24cc06c4
Pydi <pydiv@3c22fb203886.ant.amazon.com>
2020-08-09 01:22:51 -0400

    changes to main.go

--- src/carts/src/carts-service/main.go
+++ src/carts/src/carts-service/main.go
@@ -1,14 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-
 package main
 
 import (
 	"log"
 	"net/http"
+	"github.com/aws/aws-xray-sdk-go/xray"
 )
 
 func main() {
 	router := NewRouter()
+	http.Handle("/", xray.Handler(xray.NewFixedSegmentNamer("CartsService"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("Welcome to the Carts Web Service"!"))
+	})))
 	log.Fatal(http.ListenAndServe(":80", router))
 }
